be09245703ef639552c91ed4d594d2298d29563c
kaibarnhart <143548936+kaibarnhart@users.noreply.github.com>
2025-01-28 10:59:10 -0600

    Update p7.c

--- p7.c
+++ p7.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
 
+int main() {
+    int temp;
+    int *temp_ptr;
+
+    // prompts user for input
+    printf("Enter integer:\n");
+    scanf("%d", &temp);
+
+    // sets the pointer to adress of temp 
+    temp_ptr = &temp;
+
+    printf("%d\n", temp);              // The value of ‘temp’ using ‘temp’
+    printf("%d\n", *temp_ptr);         // The value of ‘temp’ using the dereferenced ‘temp_ptr’
+    printf("%p\n", (void *)&temp);     // The address of ‘temp’ using the reference of ‘temp’
+    printf("%p\n", (void *)temp_ptr);  // The address of ‘temp’ using ‘temp_ptr’
+
+    return 0;
+}
