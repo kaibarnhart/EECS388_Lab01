f7f023ee2ac7362969aaf6bdeea3084e860d9a6f
kaibarnhart <143548936+kaibarnhart@users.noreply.github.com>
2025-01-27 22:51:46 -0600

    Update p1.c

--- p1.c
+++ p1.c
@@ -4,25 +4,25 @@ int main() {
     int operand1, operand2, result;
     char operation;
 
-    // Prompt for the first operand
+    // Prompts the first operand
     printf("Enter 1st operand:\n");
     scanf("%d", &operand1);
 
-    // Prompt for the operation
-    printf("Enter operation (+, -, *, /, s):\n");
-    scanf(" %c", &operation); // Note the space before %c to ignore newline characters
+    // Prompts for the operation
+    printf("Enter operation:\n");
+    scanf(" %c", &operation);
 
     // Check the operation type
     if (operation == 's') {
-        // Perform squaring
+        // Squares
         result = operand1 * operand1;
         printf("%d\n", result);
     } else {
-        // Prompt for the second operand
+        // Prompt for second operand
         printf("Enter 2nd operand:\n");
         scanf("%d", &operand2);
 
-        // Perform the operation
+        // Performs  operation
         switch (operation) {
             case '+':
                 result = operand1 + operand2;
