4119772858c5d7b03ad982a5bdf5e137301112b2
kaibarnhart <143548936+kaibarnhart@users.noreply.github.com>
2025-02-27 21:41:30 -0600

    Create MyLinkedList_k298b686.h

---
+++ MyLinkedList_k298b686.h
@@ -0,0 +1,166 @@
+#ifndef _MY_LINKEDLIST_H_
+#define _MY_LINKEDLIST_H_
+
+#include <iostream>
+#include <algorithm>
+#include <stdexcept>
+
+template <typename DataType>
+class MyLinkedList {
+private:
+    struct Node {
+        DataType data;
+        Node* prev;
+        Node* next;
+
+        Node(const DataType& d = DataType{}, Node* p = nullptr, Node* n = nullptr)
+            : data{d}, prev{p}, next{n} {}
+    };
+
+    int theSize;
+    Node* head;
+    Node* tail;
+
+    void init() {
+        theSize = 0;
+        head = new Node;
+        tail = new Node;
+        head->next = tail;
+        tail->prev = head;
+    }
+
+public:
+    class const_iterator {
+    protected:
+        Node* current;
+        const_iterator(Node* p) : current{p} {}
+        friend class MyLinkedList<DataType>;
+
+    public:
+        const_iterator() : current{nullptr} {}
+        const DataType& operator*() const { return current->data; }
+        const_iterator& operator++() { current = current->next; return *this; }
+        const_iterator operator++(int) { const_iterator old = *this; ++(*this); return old; }
+        const_iterator& operator--() { current = current->prev; return *this; }
+        const_iterator operator--(int) { const_iterator old = *this; --(*this); return old; }
+        bool operator==(const const_iterator& rhs) const { return current == rhs.current; }
+        bool operator!=(const const_iterator& rhs) const { return !(*this == rhs); }
+    };
+
+    class iterator : public const_iterator {
+    protected:
+        iterator(Node* p) : const_iterator{p} {}
+        friend class MyLinkedList<DataType>;
+
+    public:
+        iterator() {}
+        DataType& operator*() { return this->current->data; }
+        iterator& operator++() { this->current = this->current->next; return *this; }
+        iterator operator++(int) { iterator old = *this; ++(*this); return old; }
+        iterator& operator--() { this->current = this->current->prev; return *this; }
+        iterator operator--(int) { iterator old = *this; --(*this); return old; }
+    };
+
+    MyLinkedList() { init(); }
+    ~MyLinkedList() { clear(); delete head; delete tail; }
+
+    iterator begin() { return iterator(head->next); }
+    iterator end() { return iterator(tail); }
+    int size() const { return theSize; }
+    bool empty() const { return size() == 0; }
+
+    void clear() {
+        while (!empty()) pop_front();
+    }
+
+    void push_front(const DataType& x) {
+        insert(begin(), x);
+    }
+    void push_back(const DataType& x) {
+        insert(end(), x);
+    }
+    void pop_front() {
+        if (!empty()) erase(begin());
+    }
+    void pop_back() {
+        if (!empty()) erase(--end());
+    }
+
+    iterator insert(iterator itr, const DataType& x) {
+        Node* p = itr.current;
+        Node* newNode = new Node(x, p->prev, p);
+        p->prev->next = newNode;
+        p->prev = newNode;
+        ++theSize;
+        return iterator(newNode);
+    }
+
+    iterator erase(iterator itr) {
+        if (itr.current == head || itr.current == tail) return itr;
+        Node* p = itr.current;
+        iterator retVal(p->next);
+        p->prev->next = p->next;
+        p->next->prev = p->prev;
+        delete p;
+        --theSize;
+        return retVal;
+    }
+
+    void reverseList() {
+        if (empty() || theSize == 1) return;
+        Node* current = head->next;
+        Node* temp = nullptr;
+        while (current != tail) {
+            temp = current->prev;
+            current->prev = current->next;
+            current->next = temp;
+            current = current->prev;
+        }
+        std::swap(head->next, tail->prev);
+        head->next->prev = head;
+        tail->prev->next = tail;
+    }
+
+    MyLinkedList<DataType>& appendList(MyLinkedList<DataType>&& rlist) {
+        if (rlist.empty()) return *this;
+        for (iterator itr = rlist.begin(); itr != rlist.end(); ++itr)
+            push_back(*itr);
+        return *this;
+    }
+
+    bool swapAdjElements(iterator& itr) {
+        if (itr.current == tail || itr.current->next == tail) return false;
+        Node* first = itr.current;
+        Node* second = first->next;
+        first->next = second->next;
+        second->prev = first->prev;
+        first->prev->next = second;
+        second->next->prev = first;
+        second->next = first;
+        first->prev = second;
+        itr = iterator(second);
+        return true;
+    }
+
+    DataType& front() {
+        if (!empty()) return head->next->data;
+        throw std::runtime_error("List is empty");
+    }
+
+    const DataType& front() const {
+        if (!empty()) return head->next->data;
+        throw std::runtime_error("List is empty");
+    }
+
+    DataType& back() {
+        if (!empty()) return tail->prev->data;
+        throw std::runtime_error("List is empty");
+    }
+
+    const DataType& back() const {
+        if (!empty()) return tail->prev->data;
+        throw std::runtime_error("List is empty");
+    }
+};
+
+#endif
